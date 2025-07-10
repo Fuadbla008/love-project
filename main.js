536d9020db56e7234ca343effb7359e5256accb0
fuadbla008 <171414330+Fuadbla008@users.noreply.github.com>
2025-07-10 21:55:10 +0600

    added button

---
+++ main.js
@@ -0,0 +1,24 @@
+
+
+import './ShinyText.css';
+
+const ShinyText = ({ text, disabled = false, speed = 5, className = '' }) => {
+  const animationDuration = `${speed}s`;
+
+  return (
+    <div
+      className={`shiny-text ${disabled ? 'disabled' : ''} ${className}`}
+      style={{ animationDuration }}
+    >
+      {text}
+    </div>
+  );
+};
+
+export default ShinyText;
+
+
+
+import ShinyText from './ShinyText';
+  
+<ShinyText text="Just some shiny text!" disabled={false} speed={3} className='custom-class' />
