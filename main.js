f5879b2c335667eafa9ae4a6a0fdd01ab0391020
fuadbla008 <171414330+Fuadbla008@users.noreply.github.com>
2025-08-02 20:53:28 +0600

    update new item

--- main.js
+++ main.js
@@ -24,21 +24,28 @@
 
 
 
-    // button style
+// button style
 
-    const noBtn = document.getElementById('noBtn');
-    const container = document.getElementById('container2');
+// const noBtn = document.getElementById('noBtn');
+// const container = document.getElementById('container2');
 
-    noBtn.addEventListener('mouseover', () => {
-      const containerRect = container.getBoundingClientRect();
-      const btnRect = noBtn.getBoundingClientRect();
+// noBtn.addEventListener('mouseover', () => {
+//   const containerRect = container.getBoundingClientRect();
+//   const btnRect = noBtn.getBoundingClientRect();
 
-      const maxX = container.clientWidth - noBtn.offsetWidth;
-      const maxY = container.clientHeight - noBtn.offsetHeight;
+//   const maxX = container.clientWidth - noBtn.offsetWidth;
+//   const maxY = container.clientHeight - noBtn.offsetHeight;
 
-      const newX = Math.floor(Math.random() * maxX);
-      const newY = Math.floor(Math.random() * maxY);
+//   const newX = Math.floor(Math.random() * maxX);
+//   const newY = Math.floor(Math.random() * maxY);
 
-      noBtn.style.left = newX + "px";
-      noBtn.style.top = newY + "px";
-    });
+//   noBtn.style.left = newX + "px";
+//   noBtn.style.top = newY + "px";
+// });
+
+
+let btn = document.getElementById('noBtn');
+btn.addEventListener('click', function(){
+  const remove = document.getElementsByClassName('sec-con');
+  
+})
